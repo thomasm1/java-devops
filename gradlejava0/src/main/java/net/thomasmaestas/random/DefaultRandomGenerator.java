8b48dccee6169c4c349007ecf6818417ffacb262
thomas maestas <thomasm1.maestas@gmail.com>
2020-11-18 23:57:00 -0500

    git add . ADDED/COMMIT ALL FILES HERE git add . gradlejav0 fix

---
+++ gradlejava0/src/main/java/net/thomasmaestas/random/DefaultRandomGenerator.java
@@ -0,0 +1,22 @@
+package net.thomasmaestas.random;
+
+import org.apache.commons.math3.random.RandomDataGenerator;
+
+/**
+ *
+ *
+ */
+public class DefaultRandomGenerator implements RandomGenerator
+{
+
+    public String name()
+    {
+	return "Default Randon Number Generator";
+    }
+
+    public int generate()   {
+	final RandomDataGenerator aRandomDataGenerator = new RandomDataGenerator();
+	return aRandomDataGenerator.nextInt(1, 10);
+    }
+
+}
