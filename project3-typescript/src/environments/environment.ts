ff267a03d9a548700e1f9f0e3f3954b793db551b
thomas maestas <thomasm1.maestas@gmail.com>
2020-11-26 00:42:57 -0500

    new host IP address update

--- project3-typescript/src/environments/environment.ts
+++ project3-typescript/src/environments/environment.ts
@@ -4,7 +4,7 @@
  * `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
  */
 
-const baseUri = `http://54.174.82.153`;
+const baseUri = `http://52.70.39.146`;
 /**
  * Set the port to 8080
  */
