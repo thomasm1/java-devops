b03fa5e71b6af35215eeb8e9804c9efb603699d6
thomas maestas <thomasm1.maestas@gmail.com>
2020-12-16 20:01:43 -0500

    doggywood: geckodriver & protractor tests

--- project2/src/test/java/cucumber/steps/Login.java
+++ project2/src/test/java/cucumber/steps/Login.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
--- project2/src/test/java/cucumber/steps/RegisterDoggy.java
+++ project2/src/test/java/cucumber/steps/RegisterDoggy.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
