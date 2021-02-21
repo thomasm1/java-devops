950ecd4c49a0afd772606b6abadd599cabda3f64
thomas maestas <thomasm1.maestas@gmail.com>
2021-02-20 22:32:04 -0500

    mavenCrypto

---
+++ mavenCrypto/src/main/java/net/mavencrypto/crypto/CryptoFilter.java
@@ -0,0 +1,17 @@
+package net.mavencrypto.crypto;
+
+import java.util.Arrays;
+
+public class CryptoFilter {
+
+
+    private static int[] levels;
+
+
+
+    public static void getResult(int[] levels) {
+
+        System.out.println(Arrays.toString(levels));
+    }
+
+}
--- mavenCrypto/src/main/java/net/mavencrypto/crypto/GrabCrypto.java
+++ mavenCrypto/src/main/java/net/mavencrypto/crypto/GrabCrypto.java
@@ -1,53 +1,66 @@
 package net.mavencrypto.crypto;
 
- 
+import net.mavencrypto.crypto.CryptoFilter;
+
+import java.util.Arrays;
+
 public class GrabCrypto{
-	
+
 	public static void main(String[] args){
-		
-		// Create the Subject object
-		// It will handle updating all observers 
-		// as well as deleting and adding them
-		
+
+//		cryptos();
+	 filter();
+	}
+	public static void filter() {
+		int[] levels1 = {5,3,6,7,4};
+		CryptoFilter.getResult(levels1);
+	}
+
+	public static void cryptos() {
+		/*
+		 Create the Subject object
+		 It will handle updating all observers
+		 as well as deleting and adding them
+		*/
+
 		CryptoGrabber cryptoGrabber = new CryptoGrabber();
-		
+
 		// Create an Observer that will be sent updates from Subject
-		
+
 		CryptoObserver observer1 = new CryptoObserver(cryptoGrabber);
-		
+
 		cryptoGrabber.setETHPrice(1197);
 		cryptoGrabber.setBTCPrice(33060);
 		cryptoGrabber.setHEXPrice(.01240);
-		
+
 		CryptoObserver observer2 = new CryptoObserver(cryptoGrabber);
-		
+
 		cryptoGrabber.setETHPrice(1197);
 		cryptoGrabber.setBTCPrice(33060);
 		cryptoGrabber.setHEXPrice(.01240);
-		
+
 		// Delete one of the observers
-		
+
 		// cryptoGrabber.unregister(observer2);
-		
+
 		cryptoGrabber.setETHPrice(1197);
 		cryptoGrabber.setBTCPrice(33060);
 		cryptoGrabber.setHEXPrice(.01240);
-		
+
 		// Create 3 threads using the Runnable interface
-		// GetTheCrypto implements Runnable, so it doesn't waste 
+		// GetTheCrypto implements Runnable, so it doesn't waste
 		// its one extendable class option
-		
+
 		Runnable getETH = new GetTheCrypto(cryptoGrabber, 2, "ETH", 1197);
 		Runnable getBTC = new GetTheCrypto(cryptoGrabber, 2, "BTC", 33060);
 		Runnable getHEX = new GetTheCrypto(cryptoGrabber, 2, "HEX", .01240);
-		
-		// Call for the code in run to execute 
-		
+
+		// Call for the code in run to execute
+
 		new Thread(getETH).start();
 		new Thread(getBTC).start();
 		new Thread(getHEX).start();
-		
-		
+
 	}
 	
 }
