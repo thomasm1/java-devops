52ba2fa06ead1fddffa5058bc1fc3de229b2f393
thomas maestas <thomasm1.maestas@gmail.com>
2021-02-22 23:56:58 -0500

    Arrays & Lists Conversions & 2d, 3d arrays

--- mavenAlgorithms/src/algoBasic/sortArrayAndList/App.java
+++ mavenAlgorithms/src/algoBasic/sortArrayAndList/App.java
@@ -1,6 +1,9 @@
 package algoBasic.sortArrayAndList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class SortArrays {
 
@@ -9,6 +12,26 @@ class SortArrays {
         int[] numbers = {5, 10, 2};
         SortingArrays sa = new SortingArrays();
         sa.sort(numbers);
+
+        //////////// SORTING LISTS ////////////
+        System.out.println("////// SORTING LISTS ///");
+
+        List<String> numsList = new ArrayList<>();
+        numsList.add("500");
+        numsList.add("10");
+        numsList.add("9");
+        numsList.add("50");
+        numsList.add("40");
+
+        System.out.println(numsList);
+        Collections.sort(numsList);
+        System.out.println(numsList);
+
+        List<Integer> numbs = Arrays.asList(5, 4, 8, 10, 11, 7, 3);
+        System.out.println(numbs);
+
+        Collections.sort(numbs);
+        System.out.println(numbs);
     }
 }
 
