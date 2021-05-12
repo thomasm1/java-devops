2778fd549d9cb3d6d4f5cf21a6c7fa58f4f54b5e
thomas maestas <thomasm1.maestas@gmail.com>
2021-05-11 23:45:13 -0400

    remove errors in POM

--- mavenSSL/src/main/java/us/cryptomaven/services/ProductServiceImpl.java
+++ mavenSSL/src/main/java/us/cryptomaven/services/ProductServiceImpl.java
@@ -28,6 +28,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product getProduct(Integer id) {
         jmsTextMessageService.sendTextMessage("Fetching Product ID: " + id );
+//        return productRepository.findById(id).orElse(null);
         return productRepository.findOne(id);
     }
 
