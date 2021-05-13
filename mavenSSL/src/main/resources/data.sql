0ddd4665a5ef15a1a8b5a18b903a868ae717f4c0
thomas maestas <thomasm1.maestas@gmail.com>
2021-05-12 23:32:33 -0400

    mavenSSL: schema & configuration

---
+++ mavenSSL/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (111, 'Ethereum', 'ETH', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (222, 'Bitcoin', 'BTC', 'coin', 35.08, 26.88, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (333, 'Hex', 'HEX', 'erc20Token', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (444, 'ChainLink', 'LINK', 'erc20Token', 35.08, 26.88, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (555, 'Ripple', 'XRP', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (666, 'Cardano', 'ADA', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (777, 'Aave', 'AAVE', 'erc20Token', 35.08, 26.88, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (888, 'Stellar', 'XLM', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (999, 'Neo', 'NEO', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (1111, 'Monero', 'XMR', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (1222, 'Compound', 'COMP', 'erc20Token', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (1333, 'Litecoin', 'LTC', 'coin', 94.83, 66.83, 120005);
+INSERT INTO Product (id, name, symbol, category, retail_price, discounted_price, volume) VALUES
+  (1444, 'Yearn', 'YFI', 'coin', 94.83, 66.83, 120005);
