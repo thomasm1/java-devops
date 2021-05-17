761f1b51393991099af97457ee969a51ab47de73
thomas maestas <thomasm1.maestas@gmail.com>
2021-05-16 23:56:46 -0400

    MavenSSL: planned data & data structure prototype

--- mavenSSL/src/main/resources/data.sql
+++ mavenSSL/src/main/resources/data.sql
@@ -1,29 +1,29 @@
 
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (111, 'Ethereum', 'ETH', 'coin', 94.83, 66.83, 120005);
+  (111, 'Ethereum2', 'ETH', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (222, 'Bitcoin', 'BTC', 'coin', 35.08, 26.88, 120005);
+  (222, 'Bitcoin2', 'BTC', 'coin', 35.08, 26.88, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (333, 'Hex', 'HEX', 'erc20Token', 94.83, 66.83, 120005);
+  (333, 'Hex2', 'HEX', 'erc20Token', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (444, 'ChainLink', 'LINK', 'erc20Token', 35.08, 26.88, 120005);
+  (444, 'ChainLink2', 'LINK', 'erc20Token', 35.08, 26.88, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (555, 'Ripple', 'XRP', 'coin', 94.83, 66.83, 120005);
+  (555, 'Ripple2', 'XRP', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (666, 'Cardano', 'ADA', 'coin', 94.83, 66.83, 120005);
+  (666, 'Cardano2', 'ADA', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (777, 'Aave', 'AAVE', 'erc20Token', 35.08, 26.88, 120005);
+  (777, 'Aave2', 'AAVE', 'erc20Token', 35.08, 26.88, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (888, 'Stellar', 'XLM', 'coin', 94.83, 66.83, 120005);
+  (888, 'Stellar2', 'XLM', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (999, 'Neo', 'NEO', 'coin', 94.83, 66.83, 120005);
+  (999, 'Neo2', 'NEO', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (1111, 'Monero', 'XMR', 'coin', 94.83, 66.83, 120005);
+  (1111, 'Monero2', 'XMR', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (1222, 'Compound', 'COMP', 'erc20Token', 94.83, 66.83, 120005);
+  (1222, 'Compound2', 'COMP', 'erc20Token', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (1333, 'Litecoin', 'LTC', 'coin', 94.83, 66.83, 120005);
+  (1333, 'Litecoin2', 'LTC', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-  (1444, 'Yearn', 'YFI', 'coin', 94.83, 66.83, 120005);
+  (1444, 'Yearn2', 'YFI', 'coin', 94.83, 66.83, 120005);
 INSERT INTO Product (id, coin_name, symbol, category, retail_price, discounted_price, volume) VALUES
-(1555, 'Polygon', 'MATIC', 'coin', 94.83, 66.83, 120005);
+(1555, 'Polygon2', 'MATIC', 'coin', 94.83, 66.83, 120005);
